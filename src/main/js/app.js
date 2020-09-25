ad199e22452c4065961060ffcadca803d92e50d9
Choro <oljekechoro@mail.ru>
2020-09-25 16:07:26 +0300

    feat: added version printing on start

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,9 +1,10 @@
 import log from './log'
 import Orchestrator from './orchestrator'
+import packageJson from '../../../package.json'
 
 export default class App {
   static main (argv) {
-    log.info('Qorsproxy is loading...')
+    log.info(`Qorsproxy@${packageJson.version} is loading...`)
     log.info(`argv=${JSON.stringify(argv)}`)
 
     return new Orchestrator(argv)
