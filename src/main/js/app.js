af5d854b9b17e4c6f9a149885de202dfb05b9e1c
Anton Golub <antongolub@antongolub.com>
2021-06-17 14:41:59 +0300

    refactor: separate cli, cjs and mjs entry points
    BREAKING CHANGE: repacked as mjs module

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -2,7 +2,7 @@ import log from './log'
 import Orchestrator from './orchestrator'
 import packageJson from '../../../package.json'
 
-export default class App {
+export class App {
   static main (argv) {
     log.info(`Qorsproxy@${packageJson.version} is loading...`)
     log.info(`argv=${JSON.stringify(argv)}`)
@@ -10,3 +10,5 @@ export default class App {
     return new Orchestrator(argv)
   }
 }
+
+export default App
