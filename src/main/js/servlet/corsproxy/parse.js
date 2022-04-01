e51fe198edb12a6571e85a6f146e2a0aee5a1289
Anton Golub <mailbox@antongolub.ru>
2022-04-02 00:05:42 +0300

    refactor: rm duplicated code

---
+++ src/main/js/servlet/corsproxy/parse.js
@@ -0,0 +1,23 @@
+import basicAuth from 'basic-auth'
+import crypto from 'crypto'
+import url from './url.js'
+
+export default (req) => {
+  const path = url.parseRequest(req)
+  const auth = basicAuth(req) || {}
+  const user = auth.name
+  const password = auth.pass
+  const secret = user && crypto.createHash('md5').update(user + ':' + password).digest('hex')
+  const host = path.host
+  const origin = req.get('origin')
+  const from = (origin && url.parse(origin).hostname) || origin
+
+  return {
+    path,
+    to: host,
+    from, // TODO req.ip as fallback?
+    user,
+    secret,
+    origin
+  }
+}
