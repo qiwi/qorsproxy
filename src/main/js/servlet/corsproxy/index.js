0339094f7c399e2a395497c756ce1b618efb172c
Anton Golub <mailbox@antongolub.ru>
2022-04-02 00:05:42 +0300

    refactor: rm duplicated code

--- src/main/js/servlet/corsproxy/index.js
+++ src/main/js/servlet/corsproxy/index.js
@@ -1,8 +1,7 @@
 import express from 'express'
 import crypto from 'crypto'
-import basicAuth from 'basic-auth'
 
-import url from './url.js'
+import parse from './parse.js'
 import Stats from './stats.js'
 import Rules from './rules.js'
 import {
@@ -103,22 +102,6 @@ export default class Server {
   }
 
   static parse (req) {
-    const path = url.parseRequest(req)
-    const auth = basicAuth(req) || {}
-    const user = auth.name
-    const password = auth.pass
-    const secret = user && crypto.createHash('md5').update(user + ':' + password).digest('hex')
-    const host = path.host
-    const origin = req.get('origin')
-    const from = (origin && url.parse(origin).hostname) || origin
-
-    return {
-      path,
-      to: host,
-      from, // TODO req.ip as fallback?
-      user,
-      secret,
-      origin
-    }
+    return parse(req)
   }
 }
--- src/main/js/servlet/corsproxy/middlewares/parse.js
+++ src/main/js/servlet/corsproxy/middlewares/parse.js
@@ -1,19 +1,10 @@
-import crypto from 'crypto'
-import basicAuth from 'basic-auth'
-import url from '../url.js'
+import parse from '../parse.js'
 
 export default (req, res, next) => {
-  const path = url.parseRequest(req)
-  const auth = basicAuth(req) || {}
-  const user = auth.name
-  const password = auth.pass
-  const secret = user && crypto.createHash('md5').update(user + ':' + password).digest('hex')
-  const host = path.host
-  let origin = req.get('origin')
-  origin = (origin && url.parse(origin).hostname) || origin
+  const {from ,to, secret} = parse(req)
 
-  req.to = host
-  req.from = origin
+  req.to = to
+  req.from = from
   req.secret = secret
 
   next()
